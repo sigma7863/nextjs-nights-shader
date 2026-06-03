8158863f7bd061ce81fbab0f2aeb7b78a5cc0690
matiasngf <matiasngf@hotmail.com>
2026-06-03 13:20:41 -0300

    add OpenInV0Button component and integrate it into the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import type { JSX } from 'react';
 import { Galaxy } from '@/components/galaxy/galaxy';
+import { OpenInV0Button } from '@/components/open-in-v0-button';
 
 export default function Page(): JSX.Element {
   return (
     <div className="relative text-white bg-black min-h-lvh">
       <Galaxy />
+      <div className="fixed left-4 top-4 z-10">
+        <OpenInV0Button url="https://github.com/vercel-labs/nextjs-nights-shader" />
+      </div>
     </div>
   );
 }
